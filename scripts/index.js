bcff894cfc2db028cf27d55e951f490562f5728d
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2022-05-07 22:45:42 +0300

    add redirection

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
 let loginForm = document.getElementById("login-form");
 document.querySelector(".enter").addEventListener("click", loginIntoPlayer);
+const server = "http://127.0.0.1:5000";
 
 
 function loginIntoPlayer() {
-    fetch('http://127.0.0.1:5000/user/login', {
+    fetch(`${server}/user/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
@@ -20,6 +21,7 @@ function loginIntoPlayer() {
         .then((data) => {
             if (data.hasOwnProperty('token')){
                 localStorage.setItem('token', data.token);
+                window.location.replace("./index.html");
             } else {
                 localStorage.removeItem('token');
             }
