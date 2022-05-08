f23970b44f1ce24b4f9131bdcc82e438ba2aeaad
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2022-05-08 13:09:03 +0300

    fix syntax in registration.js

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,60 +1,60 @@
-let registerForm = document.getElementById("register-form");
-document.querySelector(".enter").addEventListener("click", registerIntoPlayer);
-const server = "http://127.0.0.1:5000";
+const registerForm = document.getElementById('register-form');
+const server = 'http://127.0.0.1:5000';
 
-clearTokenAndId();
+function clearTokenAndId() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('id');
+}
+
+function closeErrorMessage() {
+    document.querySelector('.alert').style.display = 'none';
+    document.querySelector('.objectholder').style.display = 'block';
+}
+
+function showErrorMessage(error) {
+    const errorMessage = document.querySelector('.alert');
+    errorMessage.innerHTML = `<span class="closebtn">&times;</span> 
+                            <strong>Error!</strong> ${error}`;
+    errorMessage.style.display = 'block';
+    document.querySelector('.objectholder').style.display = 'none';
+    document.querySelector('.closebtn').addEventListener('click', closeErrorMessage);
+}
 
 function registerIntoPlayer() {
     fetch(`${server}/user`, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
         },
-        body: JSON.stringify({username: registerForm.username.value, password: registerForm.passwordOne.value, 
-                                email: registerForm.email.value})
-        })
+        body: JSON.stringify({
+            username: registerForm.username.value,
+            password: registerForm.passwordOne.value,
+            email: registerForm.email.value,
+        }),
+    })
         .then((response) => {
             if (!response.ok) {
                 throw Error(response.statusText);
             } else {
-                return(response.json());
+                return response.json();
             }
         })
         .then((data) => {
-            if (data.hasOwnProperty('token')){
+            if (Object.prototype.hasOwnProperty.call(data, 'token')) {
                 localStorage.setItem('token', data.token);
                 localStorage.setItem('id', data.id);
-                window.location.replace("./index.html");
+                window.location.replace('./index.html');
             } else {
                 localStorage.removeItem('token');
                 localStorage.removeItem('id');
             }
         })
-        .catch(error => {
+        .catch((error) => {
             localStorage.removeItem('token');
             localStorage.removeItem('id');
             showErrorMessage(error);
         });
-};
+}
 
-
-function clearTokenAndId() {
-    localStorage.removeItem('token');
-    localStorage.removeItem('id');
-};
-
-
-function closeErrorMessage() {
-    document.querySelector(".alert").style.display="none";
-    document.querySelector(".objectholder").style.display="block";
-};
-
-
-function showErrorMessage(error) {
-    let errorMessage = document.querySelector(".alert");
-    errorMessage.innerHTML = `<span class="closebtn">&times;</span> 
-                            <strong>Error!</strong> ${error}`;
-    errorMessage.style.display='block';
-    document.querySelector(".objectholder").style.display="none";
-    document.querySelector(".closebtn").addEventListener("click", closeErrorMessage);
-};
+document.querySelector('.enter').addEventListener('click', registerIntoPlayer);
+clearTokenAndId();
