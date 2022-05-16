07eb55a45dda1cdd40ed25938e3bdfafed0c5dc9
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2022-05-16 20:53:42 +0300

    change component to function

--- app/src/Login.js
+++ app/src/Login.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-class Login extends Component {
-    state = { 
-        username: '1',
-        password: '1',
-        error: false
-     } 
+function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    // const [error, setError] = useState(false);
 
-    constructor() {
-        super();
-        this.handleChangeUsername = this.handleChangeUsername.bind(this);
-        this.handleChangePassword = this.handleChangePassword.bind(this);
-        this.logginAttempt = this.logginAttempt.bind(this);
-    }
-
-    cancelLoggin = () => {
+    let cancelLoggin = () => {
         localStorage.removeItem('token');
         localStorage.removeItem('id');
     }
@@ -26,69 +18,66 @@ class Login extends Component {
     //     console.log('login attempt!');
     // }
 
-    handleChangeUsername(event) {
-        this.setState({username: event.target.value});
+    const handleChangeUsername = (event) => {
+        setUsername(event.target.value);
     }
 
-    handleChangePassword(event) {
-        this.setState({password: event.target.value});
+    const handleChangePassword = (event) => {
+        setPassword(event.target.value);
     }
 
-    logginAttempt() {
+    let navigate = useNavigate();
+
+    const logginAttempt = () => {
         fetch("http://127.0.0.1:5000/user/login", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify({
-                username: this.state.username,
-                password: this.state.password,
+                username: username,
+                password: password,
             }),
         })
           .then(res => res.json())
           .then(
             (result) => {
-            //   this.setState({
-            //     isLoaded: true,
-            //     items: result.items
-            //   });
-                console.log(this.state.username);   
-                console.log(this.state); 
+                console.log(username);
                 console.log(result);
                 localStorage.setItem('token', result.token);
                 localStorage.setItem('id', result.id);
-                window.location.href='/';
+                // window.location.href='/';
+                navigate("/");
             },
             // Примітка: важливо обробляти помилки саме тут,
             // а не в блоці catch (), щоб не перехоплювати
             // виключення з помилок в самих компонентах.
             (error) => {
-              this.setState({
-                error: true
-              });
+            //   this.setState({
+            //     error: true
+            //   });
+                console.log('error');
             }
           )
       }
 
-    render() { 
-        this.cancelLoggin();
-        return (
-            <div className="mainpart">
-                <h1>login</h1>
-                <div className="alert"></div>
-                <div className="objectholder">
-                    <form id="login-form">
-                        <input name="username" onChange={this.handleChangeUsername} placeholder="username" className="forminput"></input>
-                        <input name="password" onChange={this.handleChangePassword} placeholder="password" className="forminput" type="password">
-                        </input>
-                        <Link to="/" className="forget"><span>Don`t have account?</span></Link>
-                        <input onClick={this.logginAttempt} className="enter" type="button" value="submit">
-                        </input>
-                    </form>
-                </div>
+    cancelLoggin();
+    return (
+        <div className="mainpart">
+            <h1>login</h1>
+            <div className="alert"></div>
+            <div className="objectholder">
+                <form id="login-form">
+                    <input name="username" onChange={handleChangeUsername} placeholder="username" className="forminput"></input>
+                    <input name="password" onChange={handleChangePassword} placeholder="password" className="forminput" type="password">
+                    </input>
+                    <Link to="/" className="forget"><span>Don`t have account?</span></Link>
+                    <input onClick={logginAttempt} className="enter" type="button" value="submit">
+                    </input>
+                </form>
             </div>
-        );
-    }
+        </div>
+    );
 }
  
 export default Login;
