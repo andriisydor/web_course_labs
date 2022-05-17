7e55815af008214d05c8584709b30e56d9a4b8e3
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2022-05-17 15:09:29 +0300

    fix navbar

--- app/src/Navbar.js
+++ app/src/Navbar.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 
-class Navbar extends Component {
-    render() { 
-        return (
-            <div className="sidebar">
-                <Link to="/">PLAYLISTS</Link>
-                <Link to="/">SONGS</Link>
-                <Link to="/">LIBRARY</Link>
-                <Link to="/">CREATE PLAYLIST</Link>
-                <Link to="/login">LOGOUT</Link>
-                <Link to="/">EDIT ACCOUNT</Link>
-            </div>
-        );
-    }
+function Navbar() {
+    return (
+        <div className="sidebar">
+            <Link to="/">PLAYLISTS</Link>
+            <Link to="/">SONGS</Link>
+            <Link to="/">LIBRARY</Link>
+            <Link to="/">CREATE PLAYLIST</Link>
+            <Link to="/login">LOGOUT</Link>
+            <Link to="/">EDIT ACCOUNT</Link>
+        </div>
+    );
 }
  
 export default Navbar;
