43e3d89e241cc679284711647326cbed871b124a
andriisydor <andrii.sydor.knm.2020@lpnu.ua>
2022-06-13 11:15:08 +0300

    add graphics

--- app/src/Analyze.js
+++ app/src/Analyze.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Canvas from './Canvas';
 
 function Analyze() {
     return (
         <React.Fragment>
             <Navbar />
-            <div className="mainpart moveaside">
+            <div className="mainpart moveaside" style={{textAlign: 'center'}}>
                 <h3></h3>
                 <h2>analyze song</h2>
-                <h3></h3>
+                <h3 style={{color: 'red'}}>demo</h3>
+                <h3>spectrogram</h3>
                 <img src='/figure.png' style={{width: '70%'}}></img>
+                <h3>{'Frequency (Hz)'}</h3>
+                <h3>plus</h3>
+                <Canvas />
+                <h3>minus</h3>
+                <Canvas sign='minus'/>
             </div>
         </React.Fragment>
     );
